f532300c75824e62d67f09c340d42bbef763623d
ICodeInAssembly <39058370+somerandomcloud@users.noreply.github.com>
2021-04-07 19:08:53 +0200

    Owneronly cmds + updates

---
+++ commands/owner/eval.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const { botColour } = require('../../config.json')
+
+module.exports = {
+  name: "eval",
+  description: "Eval",
+  ownerOnly: true,
+  execute(message, client, args) {
+
+    try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+   
+        if (typeof evaled !== "string")
+          evaled = require("util").inspect(evaled);
+
+          const embed = new Discord.MessageEmbed()
+            .setColor(botColour)
+            .setTitle('Eval')
+            .setDescription(`\`\`\`${evaled}\`\`\``);
+   
+        message.channel.send(embed);
+      } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+      }
+
+  },
+};
