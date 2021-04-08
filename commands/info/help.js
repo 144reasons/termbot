a46db8ffb775681c3efcbe4db837a2a29cd658f5
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-08 00:39:03 -0500

    FIx
    you don't want to hide owner folder

--- commands/info/help.js
+++ commands/info/help.js
@@ -12,7 +12,6 @@ module.exports = {
       let categories = [];
 
       readdirSync("./commands/").forEach((dir) => {
-          if(dir === 'Owner') return;
         const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
           file.endsWith(".js")
         );
