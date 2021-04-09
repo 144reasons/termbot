1e5077d29e997c7e50124882b731c45359af0273
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-09 01:16:11 -0500

    adding error log thing
    This will log the errors to the channel set by the user

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const Discord = require("discord.js");
 const chalk = require("chalk");
 const fs = require("fs");
+const { inspect } = require("util")
 const { token } = require("./config.json");
+const { errorchannelID } = require("./config.json");
 
 // ──────────────────────────────────────────────────────────────────── [ Client start ]
 
@@ -43,6 +45,18 @@ process.on("SIGINT", async () => {
   console.log(chalk.bold.red("Process ended! Exiting..."));
   process.exit();
 });
+
+// ──────────────────────────────────────────────────────────────────── [ Log errors to channel that was set ]
+process.on('unhandledRejection', (reason, promise) => {
+    client.channels.cache.get(errorchannelID).send(`UnhandledRejection\nReason:\n\`\`\`\n${inspect(reason, { depth: 0 })}\n\`\`\` Promise:\n\`\`\`\n${inspect(promise, { depth: 0 })}\n\`\`\``)
+})
+process.on('uncaughtException', (err, origin) => {
+    client.channels.cache.get(errorchannelID).send(`UncaughtException\nError:\n\`\`\`\n${inspect(err, { depth: 0 })}\n\`\`\`\nType: ${inspect(origin, { depth: 0 })}`)
+})
+process.on('warning', (warn) => {
+    client.channels.cache.get(errorchannelID).send(`Warning\nWarn:\n\`\`\`\n${warn.name}\n${warn.message}\n\n${warn.stack}\n\`\`\``)
+})
+
 
 // ──────────────────────────────────────────────────────────────────── [ Login ]
 
