e00a4d121fa8e8b80eaa8560172cd2f6ccb9f034
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-11 19:48:35 -0500

    Update command.js

--- templates/command.js
+++ templates/command.js
@@ -3,7 +3,10 @@ module.exports = {
     description: "This is an example command!", // This is the command description, useful for the help command!
     ownersOnly: false, // If this is put to true, It will only allow the bot owners to use the command!
     category: "example", // This is the category the command will be put in, great for sorting your commands!
-    execute(message, client, args) { // From this line onwards, when the command is ran, the code here will run!
-      message.channel.send(`This is an example command!\nClient id: ${client.user.id}\nArguments: ${args[0]}`);
-    },
-  };
+   execute(message, client, args) {
+    // From this line onwards, when the command is ran, the code here will run!
+    message.channel.send(
+      `This is an example command!\nClient id: ${client.user.id}\nArguments: ${args[0]}`
+    );
+  },
+};
