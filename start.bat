5ec8bab2220f9bb48ccb416ae10dd7c4a04243c6
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-29 18:23:37 -0500

    Made this a cool Start.Bat file

--- start.bat
+++ start.bat
@@ -1,3 +1,28 @@
-echo "Starting"
+@echo off && color 1 && title Starting Up
+echo Please Make sure you Opened Install.bat First Then Firsttime.bat Before continuing
+pause 
+echo               ...
+echo             ;::::;
+echo           ;::::; :;
+echo         ;:::::'   :;
+echo        ;:::::;     ;.
+echo       ,:::::'       ;           OOO\
+echo       ::::::;       ;          OOOOO\
+echo       ;:::::;       ;         OOOOOOOO
+echo      ,;::::::;     ;'         / OOOOOOO
+echo    ;:::::::::`. ,,,;.        /  / DOOOOOO
+echo  .';:::::::::::::::::;,     /  /     DOOOO
+echo ,::::::;::::::;;;;::::;,   /  /        DOOO
+echo;`::::::`'::::::;;;::::: ,#/  /          DOOO
+echo:`:::::::`;::::::;;::: ;::#  /            DOOO
+echo::`:::::::`;:::::::: ;::::# /              DOO
+echo :`:::::::`;:::::: ;::::::#/               DOO
+echo :::`:::::::`;; ;:::::::::##                OO
+echo ::::`:::::::`;::::::::;:::#                OO
+echo `:::::`::::::::::::;'`:;::#                O
+echo  `:::::`::::::::;' /  / `:#
+echo   ::::::`:::::;'  /  /   `#
+echo ----------------------------------------------
+echo Starting
 node index.js
 PAUSE
