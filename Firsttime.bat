2eb5186ddd3efcf02418b52abe4e73fd14b07b0e
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-04-29 18:26:02 -0500

    Update Firsttime.bat

--- Firsttime.bat
+++ Firsttime.bat
@@ -1,3 +1,4 @@
-echo "Once this is finished open start.bat"
+@echo off && color 3 && title First Time Only Time
+echo "You Only got to do this once, After it is finished You may run start.bat"
 npm run first
 PAUSE
