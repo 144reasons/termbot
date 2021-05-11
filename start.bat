4c6b8bfd134c09b4adad664da5b857e2d5f4789e
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-10 20:45:09 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,28 +1,6 @@
 @echo off && color 1 && title Starting Up
-echo Please Make sure you Opened Install.bat First Then Firsttime.bat Before continuing
+echo Please Make sure you Opened Install.bat First Then Firsttime.bat Before continuing, If not you will have problems
 pause 
-echo               ...
-echo             ;::::;
-echo           ;::::; :;
-echo         ;:::::'   :;
-echo        ;:::::;     ;.
-echo       ,:::::'       ;           OOO\
-echo       ::::::;       ;          OOOOO\
-echo       ;:::::;       ;         OOOOOOOO
-echo      ,;::::::;     ;'         / OOOOOOO
-echo    ;:::::::::`. ,,,;.        /  / DOOOOOO
-echo  .';:::::::::::::::::;,     /  /     DOOOO
-echo ,::::::;::::::;;;;::::;,   /  /        DOOO
-echo;`::::::`'::::::;;;::::: ,#/  /          DOOO
-echo:`:::::::`;::::::;;::: ;::#  /            DOOO
-echo::`:::::::`;:::::::: ;::::# /              DOO
-echo :`:::::::`;:::::: ;::::::#/               DOO
-echo :::`:::::::`;; ;:::::::::##                OO
-echo ::::`:::::::`;::::::::;:::#                OO
-echo `:::::`::::::::::::;'`:;::#                O
-echo  `:::::`::::::::;' /  / `:#
-echo   ::::::`:::::;'  /  /   `#
-echo ----------------------------------------------
-echo Starting
+echo Starting....
 node index.js
 PAUSE
