2980260a053b3e32772eb5fd7de48b52667a09f4
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-13 12:01:18 -0500

    Now checking for config.json

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-@echo off && color 1 && title Starting Up
+@echo off && color 1 && title Starting Up..
 echo:
 if exist ./config.json (echo: Starting) else (echo: Hey, You don't seem to have a config file. If you want to run the bot please run Firsttime.bat)
 if exist ./config.json (call) else (choice /d y /t 5 > nul)
