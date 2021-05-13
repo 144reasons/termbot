830e8bc8caa9632e1f3d8a976e48a86239e4a0d6
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-13 12:01:45 -0500

    Also checking for config.json

--- Firsttime.bat
+++ Firsttime.bat
@@ -1,4 +1,7 @@
 @echo off && color 3 && title First Time Only Time
-echo "You Only got to do this once, After it is finished You may run start.bat"
+if exist ./config.json (echo: You already have a Config File You don't need more than one!) else (call)
+if exist ./config.json (choice /d y /t 5 > nul) else (call)
+if exist ./config.json (EXIT) else (call)
+echo You Only got to do this once, After it is finished You may run start.bat
 npm run first
 PAUSE
