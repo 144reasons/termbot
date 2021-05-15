269c5be270969ffa9ebd4e2c05debe46a53ddd6f
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2021-05-15 02:38:18 -0500

    Updated Bat files (Made them more cool) (#6)
    * Made this a cool Start.Bat file
    
    * Update Install.bat
    
    * Update Firsttime.bat
    
    * Update start.bat
    
    * Update start.bat
    
    * Now checking for config.json
    
    * Also checking for config.json
    
    * Update Firsttime.bat

--- Firsttime.bat
+++ Firsttime.bat
@@ -1,3 +1,7 @@
-echo "Once this is finished open start.bat"
+@echo off && color 3 && title First Time
+if exist ./config.json (echo: You already have a Config File You don't need more than one!) else (call)
+if exist ./config.json (choice /d y /t 5 > nul) else (call)
+if exist ./config.json (EXIT) else (call)
+echo You Only got to do this once, After it is finished You may run start.bat
 npm run first
 PAUSE
--- start.bat
+++ start.bat
@@ -1,3 +1,9 @@
-echo "Starting"
+@echo off && color 1 && title Starting Up..
+echo:
+if exist ./config.json (echo: Starting) else (echo: Hey, You don't seem to have a config file. If you want to run the bot please run Firsttime.bat)
+if exist ./config.json (call) else (choice /d y /t 5 > nul)
+if exist ./config.json (call) else (EXIT)
+echo:
+pause
 node index.js
 PAUSE
